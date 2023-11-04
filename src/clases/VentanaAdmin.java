32ea62c88a1d9a29ce7d1a94ac4e647e5f4155d5
martin10ca <m.calderona@uniandes.edu.co>
2023-11-04 12:06:02 -0500

    ventana admin

--- src/clases/VentanaAdmin.java
+++ src/clases/VentanaAdmin.java
@@ -8,14 +8,28 @@ import java.awt.event.ActionListener;
 
 public class VentanaAdmin {
     JFrame frame;
+    JPanel panelSuperior;
+    JTabbedPane panelInferior;
     public VentanaAdmin() {
-        this.frame = new JFrame();
-        JFrame frame = new JFrame("Menu Administrador de Sesión");
+        this.frame = new JFrame("Menu Administrador");
+        this.panelSuperior= setPanelSuperior();
+        this.panelInferior= setPanelInferior();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(800, 600);
         frame.setLayout(new BorderLayout());
 
         // Panel superior
+        
+
+        // Panel inferior
+        
+
+        frame.add(this.panelSuperior,BorderLayout.NORTH);
+        frame.add(this.panelInferior);
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+    }
+    private static JPanel setPanelSuperior(){
         JPanel panelSuperior= new JPanel();
         panelSuperior.setBorder(BorderFactory.createLineBorder(Color.GRAY, 1));
 
@@ -29,30 +43,75 @@ public class VentanaAdmin {
         JButton cerrarSesionButton = new JButton("Cerrar Sesión");
         panelSupDere.add(cerrarSesionButton);
         panelSuperior.add(panelSupDere, BorderLayout.EAST);
+        return panelSuperior;
+    }
 
-        // Panel inferior
-        JPanel panel2= new JPanel();
-        JPanel panel3= new JPanel();
-        JPanel panel4= new JPanel();
-        JPanel panel5= new JPanel();
-        JPanel panel6= new JPanel();
+    private static JTabbedPane setPanelInferior(){
+        JTabbedPane panel1= new JTabbedPane();
+        panel1.add(menuVehiculos());
+        JTabbedPane panel2= new JTabbedPane();
+        panel2.add(menuCategorias());
+        JTabbedPane panel3= new JTabbedPane();
+        panel3.add(menuSedes());
+        JTabbedPane panel4= new JTabbedPane();
+        panel4.add(menuSeguros());
+        JTabbedPane panel5= new JTabbedPane();
+        panel5.add(menuPersonal());
+        JTabbedPane panel6= new JTabbedPane();
+        panel6.add(menuTarifasPeriodos());
         JTabbedPane panelInferior = new JTabbedPane(JTabbedPane.TOP);
 
-        panelInferior.add("Vehículos",panelVehiculos());
+        panelInferior.add("Vehículos",panel1);
         panelInferior.add("Categorías",panel2);
         panelInferior.add("Sedes",panel3);
         panelInferior.add("Seguros",panel4);
         panelInferior.add("Personal",panel5);
         panelInferior.add("Tarifas/periodos",panel6);
-
-        frame.add(panelSuperior,BorderLayout.NORTH);
-        frame.add(panelInferior);
-        frame.setLocationRelativeTo(null);
-        frame.setVisible(true);
+        panelInferior.setSelectedIndex(-1);
+        return panelInferior;
     }
 
-    private static JPanel panelVehiculos(){
-        return new JPanel();
+    private static JTabbedPane menuVehiculos(){
+        JTabbedPane menu = new JTabbedPane(JTabbedPane.LEFT);
+        menu.add("Registrar Vehículo",new JPanel());
+        menu.add("Monitorear/ Actualizar vehículo",new JPanel());
+        menu.add("Obtener visualización de alto nivel",new JPanel());
+        return menu;
+    }
+
+        private static JTabbedPane menuCategorias(){
+        JTabbedPane menu = new JTabbedPane(JTabbedPane.LEFT);
+        menu.add("Registrar Categoria",new JPanel());
+        return menu;
+    }
+
+        private static JTabbedPane menuSedes(){
+        JTabbedPane menu = new JTabbedPane(JTabbedPane.LEFT);
+        menu.add("Registrar Sede",new JPanel());
+        menu.add("Modificar Sede",new JPanel());
+        return menu;
+    }
+
+        private static JTabbedPane menuSeguros(){
+        JTabbedPane menu = new JTabbedPane(JTabbedPane.LEFT);
+        menu.add("Registrar Seguro",new JPanel());
+        menu.add("Modificar Seguro",new JPanel());
+        menu.add("Eliminar Seguro",new JPanel());
+        return menu;
+    }
+        private static JTabbedPane menuPersonal(){
+        JTabbedPane menu = new JTabbedPane(JTabbedPane.LEFT);
+        menu.add("Registrar administrador local",new JPanel());
+        menu.add("Actualizar sede de un administrador local",new JPanel());
+        return menu;
+    }
+        private static JTabbedPane menuTarifasPeriodos(){
+        JTabbedPane menu = new JTabbedPane(JTabbedPane.LEFT);
+        menu.add("Actualizar costo por conductor adicional",new JPanel());
+        menu.add("Actualizar costo por traslado de vehículo",new JPanel());
+        menu.add("Actualizar periodo de temporada alta",new JPanel());
+        menu.add("Actualizar periodo de temporada baja",new JPanel());
+        return menu;
     }
         public static void main(String[] args) {
             VentanaAdmin ventana = new VentanaAdmin();
