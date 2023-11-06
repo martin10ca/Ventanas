91372fbb28df2a17a98b3350a0a9bb3d5f0d6973
jmiguelal <j.alvear@uniandes.edu.co>
2023-11-06 09:05:16 -0500

    ss

--- src/clases/crearUsuario.java
+++ src/clases/crearUsuario.java
@@ -1,27 +1,73 @@
 package clases;
 
 import java.awt.GridLayout;
+
+import javax.swing.*;
+import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JPasswordField;
+import javax.swing.JTabbedPane;
+import javax.swing.SwingUtilities;
+import javax.swing.event.DocumentListener;
+import javax.swing.event.DocumentEvent;
 
-public class crearUsuario extends JPanel {
+
+public class crearUsuario extends JFrame {
+    private JPasswordField campoContraseña;
+    private JLabel labelContraseña;
+    private JTabbedPane tabbedPane;
+    
     public crearUsuario() {
         setLayout(new GridLayout(0, 1));
-        JLabel labelMensaje = new JLabel("¡Ya falta poco! ");
+        tabbedPane = new JTabbedPane();
+        JLabel labelMensaje = new JLabel("¡Ya falta poco! Ahora toca crear un login y su contraseña.");
         JLabel labelUsuario = new JLabel("Nombre de Usuario (login): ");
         PlaceHolderTextField campoUsuario = new PlaceHolderTextField("Ej: jlopez123");
-        JLabel labelContraseña = new JLabel("Contraseña: ");
-        JPasswordField campoContraseña = new JPasswordField();
+        labelContraseña = new JLabel("Contraseña: ");
+        campoContraseña = new JPasswordField();
+        labelContraseña.setVisible(false);
+        campoContraseña.setVisible(false);
+
+        campoUsuario.getDocument().addDocumentListener((DocumentListener) new DocumentListener() {
+            @Override
+            public void insertUpdate(DocumentEvent e) {
+                mostrarCampoContraseña(!campoUsuario.getText().isEmpty());
+            }
+
+            @Override
+            public void removeUpdate(DocumentEvent e) {
+                mostrarCampoContraseña(!campoUsuario.getText().isEmpty());
+            }
+
+            @Override
+            public void changedUpdate(DocumentEvent e) {
+                mostrarCampoContraseña(!campoUsuario.getText().isEmpty());
+            }
+        });
+
+        tabbedPane.add("Usuario y Contraseña", this);
 
         add(labelMensaje);
         add(labelUsuario);
         add(campoUsuario);
 
-
-
         add(labelContraseña);
         add(campoContraseña);
+        add(tabbedPane);
+
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocation(null);
+        setSize(500, 600);
+        setVisible(true);
+
+    }
+    private void mostrarCampoContraseña(boolean mostrar) {
+        labelContraseña.setVisible(mostrar);
+        campoContraseña.setVisible(mostrar);
+    }
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> new crearUsuario());
     }
     
 }
